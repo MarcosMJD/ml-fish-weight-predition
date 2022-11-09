5aa3b05c65d0647005bf86fa10721fe77f20db4d
Marcos M. Jiménez <marcos.davia@gmail.com>
2022-11-09 11:50:59 +0100

    Single test script and updated readme

--- production/test.py
+++ production/test.py
@@ -1,9 +1,10 @@
 import requests
 import math
+from argparse import ArgumentParser
 
 # Bream	340.0	23.9	26.5	31.1	12.3778	4.6961
 
-URL = "http://fish-weight-prediction-env.eba-zarepmyp.eu-west-1.elasticbeanstalk.com:80/predict"
+DEFAULT_URI = 'http://127.0.0.1:8080/predict'
 
 y_true = 340.0
 
@@ -16,10 +17,18 @@ test_data = {
 
 if __name__ == "__main__":
 
-    r = requests.post(URL, json=test_data)
+    parser = ArgumentParser()
 
+    parser.add_argument(
+        "--uri",
+        default=DEFAULT_URI,
+        help="URL of the service's API endpoint"        
+    )
+
+    args = parser.parse_args()
+
+    r = requests.post(args.uri, json=test_data)
     predicted_weight = r.json()[0]
     print(f'Predicted weight = {predicted_weight}')
     rmse = math.sqrt((predicted_weight - y_true)**2)
     print(f'rmse = {rmse}')
-
